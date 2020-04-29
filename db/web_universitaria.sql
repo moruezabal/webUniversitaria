c7b01467144c0768630b7f53cedf4faf78965acb
Mauricio Oruezabal <moruezabal@gmail.com>
2020-04-29 11:45:15 -0300

    DB Actualizada

--- db/web_universitaria.sql
+++ db/web_universitaria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-04-2020 a las 15:56:05
+-- Tiempo de generación: 29-04-2020 a las 16:44:23
 -- Versión del servidor: 10.1.33-MariaDB
 -- Versión de PHP: 7.2.6
 
@@ -86,7 +86,23 @@ INSERT INTO `materia` (`id_materia`, `nombre`, `profesor`, `id_carrera`) VALUES
 (17, 'Algoritmos', 'Maria Del Carmen Aguilar ', 5),
 (18, 'Base de Datos', 'Elizerio Salazar', 5),
 (19, 'Ingeniería del Sotware', 'Norma Martínez', 5),
-(20, 'Orientación a Objetos', 'Roberto Garza', 5);
+(20, 'Orientación a Objetos', 'Roberto Garza', 5),
+(21, 'Análisis discursivo', 'Luis Font', 6),
+(22, 'Redacción en red', 'Roberto Ruarte', 6),
+(23, 'Sistemas de medios', 'Teresa Puente', 6),
+(24, 'Crónica Periodística', 'Dante Milán', 6),
+(25, 'Cocina mediterránea', 'Eladio Pérez', 7),
+(26, 'Francés', 'Ana Figueroa', 7),
+(27, 'Salud e Higiene', 'Tadeo Imola', 7),
+(28, 'Manipulación de Alipentos', 'Natalia Stein', 7),
+(29, 'Primeros Auxilios', 'Romina Bazán', 8),
+(30, 'Instrumentología', 'Horacio Rondón', 8),
+(31, 'Protocolo Institucional', 'Renzo Juárez', 8),
+(32, 'Anatomía Aplicada', 'Belén Cortez', 8),
+(33, 'Microeconomía', 'Natalia Soler', 9),
+(34, 'Marketing', 'Lorenzo Kwist', 9),
+(35, 'Administración', 'Bruno Sosa', 9),
+(36, 'Investigación del Mercado', 'Julieta Scopone', 9);
 
 --
 -- Índices para tablas volcadas
@@ -119,7 +135,7 @@ ALTER TABLE `carrera`
 -- AUTO_INCREMENT de la tabla `materia`
 --
 ALTER TABLE `materia`
-  MODIFY `id_materia` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id_materia` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- Restricciones para tablas volcadas
